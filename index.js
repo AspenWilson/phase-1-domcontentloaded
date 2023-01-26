de94fb64541586031a62a58dfc90f4dc8bfb67ea
Aspen Wilson <aspen.schell@gmail.com>
2023-01-26 11:52:38 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("The DOM has loaded");
+  });
+
+let text = document.getElementById('text');
+    document.addEventListener("DOMContentLoaded", (event) => {
+        text.textContent = `This is really cool!`
+    })
+
+  
